52292a2963cd4e6efe22a27c297ae0b4ed660908
Alexander Reichert <alexander@alexander-ThinkPad-T420.fritz.box>
2013-07-03 22:12:31 +0200

    minor bugfixes

--- src/com/elgubbo/tastekid/SectionFragment.java
+++ src/com/elgubbo/tastekid/SectionFragment.java
@@ -1,20 +1,25 @@
 package com.elgubbo.tastekid;
 
 import java.util.ArrayList;
-import java.util.List;
-
 import com.elgubbo.tastekid.interfaces.IQueryCompleteListener;
 import com.elgubbo.tastekid.model.ApiResponse;
 import com.elgubbo.tastekid.model.Result;
 
+import android.app.ActionBar.LayoutParams;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class SectionFragment extends Fragment implements IQueryCompleteListener {
@@ -25,6 +30,14 @@ public class SectionFragment extends Fragment implements IQueryCompleteListener 
 	Context appContext;
 	ListView listView;
 	CardListArrayAdapter adapter;
+	LayoutInflater inflater;
+	
+	
+	//Layouts
+	private LinearLayout progressLayout;
+	private LinearLayout headerLayout;
+	private LinearLayout infoLayout;
+
 	int position;
 
 	public static SectionFragment init(int val, Context context) {
@@ -41,18 +54,76 @@ public class SectionFragment extends Fragment implements IQueryCompleteListener 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		this.inflater = inflater;
 		rootView = inflater.inflate(R.layout.fragment_main_layout, container,
 				false);
 		listView = (ListView) rootView.findViewById(R.id.cardListView);
 		adapter = new CardListArrayAdapter(appContext, results);
+		
+		setupHeader();
+		
 		listView.setAdapter(adapter);
-
+		
+		
+		
 		Log.d("TasteKid", "creating layout");
 		return rootView;
 	}
 
 	private void updateCards() {
+		fillHeaderInfo(info.get(0));
 		adapter.notifyDataSetChanged();
+	}
+	
+	private void setupHeader(){
+		headerLayout = new LinearLayout(appContext);
+		headerLayout.setGravity(Gravity.CENTER);
+		
+		headerLayout.setLayoutParams(new AbsListView.LayoutParams(LayoutParams.MATCH_PARENT, 
+                LayoutParams.MATCH_PARENT));
+		
+		infoLayout = (LinearLayout) inflater.inflate(R.layout.header_item, null);
+		headerLayout.addView(infoLayout);
+		infoLayout.setVisibility(View.GONE);
+
+		setupLoadingBar();
+		
+		listView.addHeaderView(headerLayout);
+	}
+	
+	public void fillHeaderInfo(Result result){
+		TextView title = (TextView) infoLayout.findViewById(R.id.title);
+		TextView description = (TextView) infoLayout.findViewById(R.id.description);
+		title.setText(result.name);
+		description.setText(result.wTeaser);
+		infoLayout.setVisibility(View.VISIBLE);
+	}
+	
+	private void setupLoadingBar(){
+		progressLayout = new LinearLayout(appContext);
+		progressLayout.setGravity(Gravity.CENTER);
+		progressLayout.setBackgroundResource(R.drawable.card_background);
+		TextView loadingText = new TextView(appContext);
+		loadingText.setText("Loading...");
+		loadingText.setTextColor(Color.BLACK);
+		loadingText.setGravity(Gravity.CENTER);
+		ProgressBar loadingProgressBar = new ProgressBar(appContext);
+		progressLayout.addView(loadingText);
+		progressLayout.addView(loadingProgressBar);
+		headerLayout.addView(progressLayout);
+		progressLayout.setVisibility(View.GONE);
+	}
+	
+	public void showLoadingBar(){
+		if(Configuration.DEVMODE)
+			Log.d("TasteKid", "showing loadingbar");
+		progressLayout.setVisibility(View.VISIBLE);
+	}
+	
+	public void hideLoadingBar(){
+		if(Configuration.DEVMODE)
+			Log.d("TasteKid", "hiding loadingbar");
+		progressLayout.setVisibility(View.GONE);
 	}
 	
 
@@ -65,6 +136,7 @@ public class SectionFragment extends Fragment implements IQueryCompleteListener 
 		adapter.clear();
 		adapter.addAll(results);
 		updateCards();
+		hideLoadingBar();
 	}
 
 	@Override
